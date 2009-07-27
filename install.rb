76ddfae820e7b1755e830eff0575ec73a800835a
Richard <richard@ekohe.com>
2009-07-27 09:39:27 +0800

    mkdir db/migrate if not exist

--- install.rb
+++ install.rb
@@ -13,6 +13,7 @@ end
 puts "** Installing chinese_regions migrations"
 migration_src_path = File.join(RAILS_ROOT, '/vendor/plugins/chinese_regions/generators/migrations/')
 migration_dest_path = File.join(RAILS_ROOT, '/db/migrate/')
+FileUtils.mkdir(migration_dest_path) unless File.exists?(migration_dest_path)
 ['create_regions.rb', 'create_cities.rb', 'create_districts.rb'].each do |file|
   FileUtils.cp(File.join(migration_src_path, file), File.join(migration_dest_path, Time.new.getutc.strftime("%Y%m%d%H%M%S") + "_" + file))
   sleep 1
